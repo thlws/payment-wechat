0b074b276219c3d34170d6caa0d81cb6737e21a5
hanley <hanley@thlws.com>
2017-11-23 20:19:08 +0800

    update ThlwsNameCoder.java

--- src/main/java/org/thlws/payment/wechat/extra/xml/ThlwsNameCoder.java
+++ src/main/java/org/thlws/payment/wechat/extra/xml/ThlwsNameCoder.java
@@ -4,8 +4,10 @@ import com.thoughtworks.xstream.io.xml.XmlFriendlyNameCoder;
 
 /**
  * 自定义Coder
- * 1.Java Bean 中包含$符号特殊处理,保证xmlToObj and ObjToxml 可以正常工作
- * 2.Java Bean 中若包含下划线 _ 时，转换(xmlToObj and ObjToxml)可正常工作
+ * <ol>
+ *     <li>Java Bean 中包含$符号特殊处理,保证xmlToObj and ObjToxml 可以正常工作</li>
+ *     <li>Java Bean 中若包含下划线 _ 时，转换(xmlToObj and ObjToxml)可正常工作</li>
+ * </ol>
  */
 public class ThlwsNameCoder extends XmlFriendlyNameCoder {
 
